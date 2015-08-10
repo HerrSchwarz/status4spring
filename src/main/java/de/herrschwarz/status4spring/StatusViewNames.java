691145d15688a1c349e99caa49366aa785e9312b
Felix Eckhardt <eckhardt@silpion.de>
2015-08-10 18:18:35 +0200

    Initial commit: StatusController with default template and a hostInspector

---
+++ src/main/java/de/herrschwarz/status4spring/StatusViewNames.java
@@ -0,0 +1,9 @@
+package de.herrschwarz.status4spring;
+
+public class StatusViewNames {
+
+
+  public static final String INTERNAL_STATUS_VIEW_NAME = "/status";
+  public static final String INTERNAL_VERSION_VIEW_NAME = "/version";
+  public static final String INTERNAL_HEALTH_VIEW_NAME = "/health";
+}
---
+++ src/test/java/de/herrschwarz/status4spring/inspectors/HostInspectorTest.java
@@ -0,0 +1,40 @@
+package de.herrschwarz.status4spring.inspectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.IOException;
+import java.net.Socket;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(SocketFactory.class)
+public class HostInspectorTest {
+
+  @Test
+  public void shouldFail() throws Exception {
+    mockStatic(SocketFactory.class);
+    when(SocketFactory.createSocket(anyString(), anyInt())).thenThrow(new IOException());
+    HostInspector testUnit = new HostInspector("test", "localhost", 0);
+    InspectionResult result = testUnit.inspect();
+    assertThat(result.isSuccessfull(), is(false));
+  }
+
+  @Test
+  public void shouldSucceed() throws Exception {
+    mockStatic(SocketFactory.class);
+    when(SocketFactory.createSocket(anyString(), anyInt())).thenReturn(mock(Socket.class));
+    HostInspector testUnit = new HostInspector("test", "localhost", 0);
+    InspectionResult result = testUnit.inspect();
+    assertThat(result.isSuccessfull(), is(true));
+  }
+}
