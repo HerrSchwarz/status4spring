2e30dc7095558bef9e191ce6bea31b1b72cadf5b
Felix Eckhardt <eckhardt@silpion.de>
2015-08-12 17:36:09 +0200

    added MongoDbInspector

---
+++ src/main/java/de/herrschwarz/status4spring/inspectors/MongoDbInspector.java
@@ -0,0 +1,58 @@
+package de.herrschwarz.status4spring.inspectors;
+
+import static org.springframework.util.StringUtils.isEmpty;
+
+import com.google.common.base.Joiner;
+import com.mongodb.CommandResult;
+import com.mongodb.DB;
+
+public class MongoDbInspector implements HealthInspector {
+
+  private static final String MONGO_STATUS = "status = ";
+  private static final String MONGO_STATUS_OK = MONGO_STATUS + "OK";
+  private static final String MONGO_STATUS_ERROR = MONGO_STATUS + "ERROR";
+  private static final String MONGO_STATUS_WARNING = MONGO_STATUS + "WARNING";
+  private String name;
+  private DB mongoClientDB;
+
+  public MongoDbInspector(String name, DB mongoClientDB) {
+    this.name = name;
+    this.mongoClientDB = mongoClientDB;
+  }
+
+  @Override
+  public InspectionResult inspect() {
+    Joiner joiner = Joiner.on(", ").skipNulls();
+    boolean mongoOk = true;
+    int numberOfCollections = 0;
+    String message = "name = ";
+    try {
+      message += mongoClientDB.getName();
+      numberOfCollections = mongoClientDB.getCollectionNames().size();
+      CommandResult stats = mongoClientDB.getStats();
+      if (!stats.ok() || !isEmpty(stats.getErrorMessage())) {
+        message = joiner.join(message, MONGO_STATUS_WARNING + " (" + stats.getErrorMessage() + ")");
+        mongoOk = false;
+      }
+      if (numberOfCollections == 0) {
+        message =  joiner.join(message, MONGO_STATUS_WARNING + " (No Collections found)");
+        mongoOk = false;
+      }
+    } catch (Exception e) {
+      message = joiner.join(message, MONGO_STATUS_ERROR + " (" + e + ")");
+      mongoOk = false;
+    }
+
+    if (mongoOk) {
+      message = joiner.join(message, MONGO_STATUS_OK);
+    }
+
+    message = joiner.join(message, "Collections = " + numberOfCollections);
+    return new InspectionResult(name, mongoOk, message);
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+}
---
+++ src/test/java/de/herrschwarz/status4spring/inspectors/MongoDbInspectorTest.java
@@ -0,0 +1,80 @@
+package de.herrschwarz.status4spring.inspectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+
+import com.mongodb.CommandResult;
+import com.mongodb.DB;
+import com.mongodb.MongoException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.HashSet;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MongoDbInspectorTest {
+
+  @Mock
+  DB mongoDb;
+
+  @Mock
+  CommandResult commandResult;
+
+  @Test
+  public void shouldDetectMissingMongoDB() throws Exception {
+    when(mongoDb.getCollectionNames()).thenThrow(new MongoException("test"));
+    when(mongoDb.getName()).thenReturn("test");
+    MongoDbInspector inspector = new MongoDbInspector("mongoDb", this.mongoDb);
+    InspectionResult result = inspector.inspect();
+    assertThat(result.isSuccessfull(), is(false));
+    assertThat(result.getDescription(),
+        is("name = test, status = ERROR (com.mongodb.MongoException: test), Collections = 0"));
+  }
+
+  @Test
+  public void shouldDetectMongoDBNotOk() throws Exception {
+    HashSet<String> collections = new HashSet<>();
+    collections.add("test");
+    when(mongoDb.getCollectionNames()).thenReturn(collections);
+    when(mongoDb.getStats()).thenReturn(commandResult);
+    when(mongoDb.getName()).thenReturn("test");
+    when(commandResult.ok()).thenReturn(false);
+    MongoDbInspector inspector = new MongoDbInspector("mongoDb", this.mongoDb);
+    InspectionResult result = inspector.inspect();
+    assertThat(result.isSuccessfull(), is(false));
+    assertThat(result.getDescription(),
+        is("name = test, status = WARNING (null), Collections = 1"));
+  }
+
+  @Test
+  public void shouldDetectMongoDBOk() throws Exception {
+    HashSet<String> collections = new HashSet<>();
+    collections.add("test");
+    when(mongoDb.getCollectionNames()).thenReturn(collections);
+    when(mongoDb.getStats()).thenReturn(commandResult);
+    when(commandResult.ok()).thenReturn(true);
+    when(mongoDb.getName()).thenReturn("test");
+    MongoDbInspector inspector = new MongoDbInspector("mongoDb", this.mongoDb);
+    InspectionResult result = inspector.inspect();
+    assertThat(result.isSuccessfull(), is(true));
+    assertThat(result.getDescription(), is("name = test, status = OK, Collections = 1"));
+  }
+
+  @Test
+  public void shouldDetectEmptyDb() throws Exception {
+    when(mongoDb.getCollectionNames()).thenReturn(new HashSet<>());
+    when(mongoDb.getStats()).thenReturn(commandResult);
+    when(commandResult.ok()).thenReturn(true);
+    when(mongoDb.getName()).thenReturn("test");
+    MongoDbInspector inspector = new MongoDbInspector("mongoDb", this.mongoDb);
+    InspectionResult result = inspector.inspect();
+    assertThat(result.isSuccessfull(), is(false));
+    assertThat(result.getDescription(),
+        is("name = test, status = WARNING (No Collections found), Collections = 0"));
+
+  }
+}
