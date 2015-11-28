88d1b325a03915461291ff046674886b0079e690
Felix Eckhardt <eckhardt@silpion.de>
2015-11-28 18:40:27 +0100

    made spring-boot ready

--- src/main/java/de/herrschwarz/status4spring/StatusViewNames.java
+++ src/main/java/de/herrschwarz/status4spring/StatusViewNames.java
@@ -3,7 +3,7 @@ package de.herrschwarz.status4spring;
 public class StatusViewNames {
 
 
-  public static final String INTERNAL_STATUS_VIEW_NAME = "/status";
-  public static final String INTERNAL_VERSION_VIEW_NAME = "/version";
-  public static final String INTERNAL_HEALTH_VIEW_NAME = "/health";
+  public static final String INTERNAL_STATUS_VIEW_NAME = "status";
+  public static final String INTERNAL_VERSION_VIEW_NAME = "version";
+  public static final String INTERNAL_HEALTH_VIEW_NAME = "health";
 }
