43ec1a17e6e4b03b6e1f5d8988d78896360c764f
Felix Eckhardt <eckhardt@silpion.de>
2015-12-02 21:44:53 +0100

    title and header links more configurable

--- src/main/java/com/github/herrschwarz/status4spring/StatusController.java
+++ src/main/java/com/github/herrschwarz/status4spring/StatusController.java
@@ -2,6 +2,7 @@ package com.github.herrschwarz.status4spring;
 
 import com.github.herrschwarz.status4spring.inspectors.HealthInspector;
 import com.github.herrschwarz.status4spring.inspectors.InspectionResult;
+import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,7 +15,12 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
+import static com.github.herrschwarz.status4spring.StatusModelKeys.CUSTOM_HEADER_ENTRIES_KEY;
+import static com.github.herrschwarz.status4spring.StatusModelKeys.HEADER_KEY;
+import static com.github.herrschwarz.status4spring.StatusModelKeys.HEALTH_KEY;
+import static com.github.herrschwarz.status4spring.StatusModelKeys.VERSION_KEY;
 import static com.github.herrschwarz.status4spring.StatusViewNames.INTERNAL_STATUS_VIEW_NAME;
 import static com.github.herrschwarz.status4spring.StatusViewNames.INTERNAL_VERSION_VIEW_NAME;
 import static java.lang.Double.valueOf;
@@ -32,95 +38,105 @@ import static org.slf4j.LoggerFactory.getLogger;
 @Controller
 public class StatusController {
 
-  private static final String HEALTH_ATTRIBUTE = "health";
-  private static final String VERSION_ATTRIBUTE = "version";
-  private static final String HEADER_ATTRIBUTE = "header";
-  private List<HealthInspector> healthInspectors = new ArrayList<>();
-  private String version;
-  private String header;
 
-  private static final Logger LOG = getLogger(lookup().lookupClass());
+    private List<HealthInspector> healthInspectors = new ArrayList<>();
+    private String version;
+    private String header;
+    private Map<String, String> customHeaderEntries = ImmutableMap.of();
 
-  public StatusController() {
-  }
+    private static final Logger LOG = getLogger(lookup().lookupClass());
 
-  @RequestMapping(value = "/internal/status") //"${internal.status.url}")
-  public ModelAndView showStatus() {
-    return new ModelAndView(INTERNAL_STATUS_VIEW_NAME);
-  }
-
-  @RequestMapping(value = "/internal/version")
-  public ModelAndView showVersion() {
-    return new ModelAndView(INTERNAL_VERSION_VIEW_NAME);
-  }
-
-  @RequestMapping(value = "/internal/health")
-  public @ResponseBody List<InspectionResult> showHealth() {
-    return inspectSystem();
-  }
-
-  @ModelAttribute("status")
-  public ModelMap statusMap() {
-    ModelMap model = new ModelMap();
-    model.put("uptime", uptime());
-    model.put("starttime", new Date(getRuntimeMXBean().getStartTime()).toString());
-    model.put("classpath", getRuntimeMXBean().getClassPath());
-    model.put("freeMemory", getRuntime().freeMemory());
-    model.put("maxMemory", getRuntime().maxMemory());
-    model.put("usedMemory", getRuntime().maxMemory() - getRuntime().freeMemory());
-    model.put("memoryUsage",
-        (getRuntime().maxMemory() - getRuntime().freeMemory()) / valueOf(getRuntime().maxMemory()));
-    model.put("averageLoad", getOperatingSystemMXBean().getSystemLoadAverage());
-    model.put("processors", getRuntime().availableProcessors());
-    model.put("threadCount", activeCount());
-    model.put("osArch", getOperatingSystemMXBean().getArch());
-    model.put("osName", getOperatingSystemMXBean().getName());
-    model.put("osVersion", getOperatingSystemMXBean().getVersion());
-    model.put("systemProperties", getRuntimeMXBean().getSystemProperties());
-    model.put("environmentVariables", getenv());
-    model.put("systemCheck", inspectSystem());
-    return model;
-  }
-
-  @ModelAttribute(VERSION_ATTRIBUTE)
-  public String version() {
-    return this.version;
-  }
-
-  @ModelAttribute(HEADER_ATTRIBUTE)
-  public String header() {
-    return this.header;
-  }
-
-  @ModelAttribute(HEALTH_ATTRIBUTE)
-  private List<InspectionResult> inspectSystem() {
-    return healthInspectors.stream().map(i -> executeInspector(i)).collect(toList());
-  }
-
-  private InspectionResult executeInspector(HealthInspector inspector) {
-    try {
-      return inspector.inspect();
-    } catch (Throwable t) {
-      LOG.error(format(ROOT, "error running healthInspector %s", inspector.getName()), t);
-      return new InspectionResult(inspector.getName(), false, "execution of inspector failed");
+    public StatusController() {
     }
-  }
 
-  private String uptime() {
-    long uptimeInMs = getRuntimeMXBean().getUptime();
-    Duration duration = Duration.ofMillis(uptimeInMs);
-    return duration.toString();
-  }
+    @RequestMapping(value = "/internal/status") //"${internal.status.url}")
+    public ModelAndView showStatus() {
+        return new ModelAndView(INTERNAL_STATUS_VIEW_NAME);
+    }
 
-  public void addHealthInspector(HealthInspector inspector) {
-    healthInspectors.add(inspector);
-  }
+    @RequestMapping(value = "/internal/version")
+    public ModelAndView showVersion() {
+        return new ModelAndView(INTERNAL_VERSION_VIEW_NAME);
+    }
 
-  public void setVersion(String version) {
-    this.version = version;
-  }
+    @RequestMapping(value = "/internal/health")
+    public
+    @ResponseBody
+    List<InspectionResult> showHealth() {
+        return inspectSystem();
+    }
 
-  public void setHeader(String header) {
-    this.header = header;
-  }
+    @ModelAttribute("status")
+    public ModelMap statusMap() {
+        ModelMap model = new ModelMap();
+        model.put("uptime", uptime());
+        model.put("starttime", new Date(getRuntimeMXBean().getStartTime()).toString());
+        model.put("classpath", getRuntimeMXBean().getClassPath());
+        model.put("freeMemory", getRuntime().freeMemory());
+        model.put("maxMemory", getRuntime().maxMemory());
+        model.put("usedMemory", getRuntime().maxMemory() - getRuntime().freeMemory());
+        model.put("memoryUsage",
+                (getRuntime().maxMemory() - getRuntime().freeMemory()) / valueOf(getRuntime().maxMemory()));
+        model.put("averageLoad", getOperatingSystemMXBean().getSystemLoadAverage());
+        model.put("processors", getRuntime().availableProcessors());
+        model.put("threadCount", activeCount());
+        model.put("osArch", getOperatingSystemMXBean().getArch());
+        model.put("osName", getOperatingSystemMXBean().getName());
+        model.put("osVersion", getOperatingSystemMXBean().getVersion());
+        model.put("systemProperties", getRuntimeMXBean().getSystemProperties());
+        model.put("environmentVariables", getenv());
+        model.put("systemCheck", inspectSystem());
+        return model;
+    }
+
+    @ModelAttribute(CUSTOM_HEADER_ENTRIES_KEY)
+    public Map customHeaderEntries() {
+        return this.customHeaderEntries;
+    }
+
+    @ModelAttribute(VERSION_KEY)
+    public String version() {
+        return this.version;
+    }
+
+    @ModelAttribute(HEADER_KEY)
+    public String header() {
+        return this.header;
+    }
+
+    @ModelAttribute(HEALTH_KEY)
+    private List<InspectionResult> inspectSystem() {
+        return healthInspectors.stream().map(i -> executeInspector(i)).collect(toList());
+    }
+
+    private InspectionResult executeInspector(HealthInspector inspector) {
+        try {
+            return inspector.inspect();
+        } catch (Throwable t) {
+            LOG.error(format(ROOT, "error running healthInspector %s", inspector.getName()), t);
+            return new InspectionResult(inspector.getName(), false, "execution of inspector failed");
+        }
+    }
+
+    private String uptime() {
+        long uptimeInMs = getRuntimeMXBean().getUptime();
+        Duration duration = Duration.ofMillis(uptimeInMs);
+        return duration.toString();
+    }
+
+    public void addHealthInspector(HealthInspector inspector) {
+        healthInspectors.add(inspector);
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public void setCustomHeaderEntries(Map<String, String> customHeaderEntries) {
+        this.customHeaderEntries = customHeaderEntries;
+    }
 }
---
+++ src/main/java/com/github/herrschwarz/status4spring/StatusModelKeys.java
@@ -0,0 +1,8 @@
+package com.github.herrschwarz.status4spring;
+
+public class StatusModelKeys {
+    public static final String HEADER_KEY = "header";
+    public static final String CUSTOM_HEADER_ENTRIES_KEY = "customHeaderEntries";
+    public static final String HEALTH_KEY = "health";
+    public static final String VERSION_KEY = "version";
+}
