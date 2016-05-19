5df4607bfc9f83eae581db34616710453b5d7aef
Felix Eckhardt <HerrSchwarz@users.noreply.github.com>
2016-05-19 20:55:34 +0200

    added test for StatusController (#17)

--- src/test/java/com/github/herrschwarz/status4spring/StatusControllerTest.java
+++ src/test/java/com/github/herrschwarz/status4spring/StatusControllerTest.java
@@ -3,14 +3,27 @@ package com.github.herrschwarz.status4spring;
 import com.github.herrschwarz.status4spring.groups.UnitTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.util.Map;
+
+import static com.github.herrschwarz.status4spring.SessionModelKeys.*;
 import static com.github.herrschwarz.status4spring.ViewNames.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsMapContaining.hasKey;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
 
 @Category(UnitTest.class)
 public class StatusControllerTest {
+
+    public static final String ATTRIBUTE_KEY = "meaningOfDeath";
+    public static final String ATTRIBUTE_VALUE = "666";
 
     @Test
     public void shouldSelectStatusViewNameForStatusPage() throws Exception {
@@ -48,4 +61,62 @@ public class StatusControllerTest {
         assertThat("View name is wrong", modelAndView.getViewName(), is(INTERNAL_BUILD_VIEW_NAME));
     }
 
+    @Test
+    public void shouldSelectSessionViewNameForSessionPage() throws Exception {
+        // Given
+        StatusController statusController = new StatusController();
+
+        // When
+        ModelAndView modelAndView = statusController.session(new MockHttpSession());
+
+        // Then
+        assertThat("View name is wrong", modelAndView.getViewName(), is(INTERNAL_SESSION_VIEW_NAME));
+    }
+
+    @Test
+    public void shouldPutSessionAttributeInModelMap() throws Exception {
+        // Given
+        StatusController statusController = new StatusController();
+        HttpSession session = new MockHttpSession();
+        session.setAttribute(ATTRIBUTE_KEY, ATTRIBUTE_VALUE);
+
+        // When
+        ModelAndView modelAndView = statusController.session(session);
+
+        // Then
+        Map<String, String> sessionAttributes = (Map<String, String>) modelAndView.getModelMap().get(SESSION_ATTRIBUTES_MODEL_KEY);
+        assertThat(sessionAttributes, not(nullValue()));
+        assertThat(sessionAttributes.get(ATTRIBUTE_KEY), is(ATTRIBUTE_VALUE));
+    }
+
+    @Test
+    public void shouldPutSessionIdInModelMap() throws Exception {
+        // Given
+        StatusController statusController = new StatusController();
+
+        // When
+        ModelAndView modelAndView = statusController.session(new MockHttpSession());
+
+        // Then
+        ModelMap modelMap = modelAndView.getModelMap();
+        assertThat(modelMap, not(nullValue()));
+        assertThat(modelMap, hasKey(SESSION_ID_MODEL_KEY));
+    }
+
+    @Test
+    public void shouldPutSessionCreationTimestampInModelMap() throws Exception {
+        // Given
+        StatusController statusController = new StatusController();
+        MockHttpSession session = new MockHttpSession();
+
+
+        // When
+        ModelAndView modelAndView = statusController.session(session);
+
+        // Then
+        ModelMap modelMap = modelAndView.getModelMap();
+        assertThat(modelMap, not(nullValue()));
+        assertThat(modelMap, hasKey(SESSION_CREATION_TIME_MODEL_KEY));
+        assertThat(modelMap.get(SESSION_CREATION_TIME_MODEL_KEY), is(new Date(session.getCreationTime()).toString()));
+    }
 }
