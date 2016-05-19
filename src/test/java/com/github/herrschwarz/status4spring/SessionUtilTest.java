7e9cbe5d6312e1fd7337d6e468779c8989a0c12b
Felix Eckhardt <eckhardt@silpion.de>
2016-05-19 19:53:36 +0200

    added test for SessionUtil

---
+++ src/test/java/com/github/herrschwarz/status4spring/SessionUtilTest.java
@@ -0,0 +1,37 @@
+package com.github.herrschwarz.status4spring;
+
+import com.github.herrschwarz.status4spring.groups.UnitTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.springframework.mock.web.MockHttpSession;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@Category(UnitTest.class)
+public class SessionUtilTest {
+
+    @Test
+    public void shouldGenerateEmptySessionAttributeMap() throws Exception {
+        HttpSession session = new MockHttpSession();
+        Map<String, String> result = SessionUtil.generateSessionAttributeMap(session);
+        assertThat(result, not(nullValue()));
+    }
+
+    @Test
+    public void shouldGenerateSessionAttributeMap() throws Exception {
+        HttpSession session = new MockHttpSession();
+        session.setAttribute("meaningOfLive", 42);
+        session.setAttribute("towel", true);
+        session.setAttribute("thanks", "for the fish");
+        Map<String, String> result = SessionUtil.generateSessionAttributeMap(session);
+        assertThat(result.get("meaningOfLive"), is("42"));
+        assertThat(result.get("towel"), is("true"));
+        assertThat(result.get("thanks"), is("for the fish"));
+    }
+}
