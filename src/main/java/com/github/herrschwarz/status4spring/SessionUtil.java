9bb807acc4c32e42a08202af2d579eb84f243658
Felix Eckhardt <eckhardt@silpion.de>
2016-05-23 19:19:02 +0200

    scoping fixed

--- src/main/java/com/github/herrschwarz/status4spring/SessionUtil.java
+++ src/main/java/com/github/herrschwarz/status4spring/SessionUtil.java
@@ -1,13 +1,14 @@
 package com.github.herrschwarz.status4spring;
 
-import javax.servlet.http.HttpSession;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 public class SessionUtil {
 
-    static Map<String, String> generateSessionAttributeMap(HttpSession session) {
+    public static Map<String, String> generateSessionAttributeMap(HttpSession session) {
         Enumeration<String> attributeNames = session.getAttributeNames();
         HashMap<String, String> sessionAttributes = new HashMap<>();
         while (attributeNames.hasMoreElements()) {
