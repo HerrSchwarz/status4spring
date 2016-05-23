0886f4c31cf3cfdf7bfdee2dfee4cb8146964979
Felix Eckhardt <eckhardt@silpion.de>
2016-05-23 19:58:01 +0200

    more testing for status controller / health inspection

--- src/test/java/com/github/herrschwarz/status4spring/StatusControllerTest.java
+++ src/test/java/com/github/herrschwarz/status4spring/StatusControllerTest.java
@@ -1,33 +1,49 @@
 package com.github.herrschwarz.status4spring;
 
+import static com.github.herrschwarz.status4spring.SessionModelKeys.SESSION_ATTRIBUTES_MODEL_KEY;
+import static com.github.herrschwarz.status4spring.SessionModelKeys.SESSION_CREATION_TIME_MODEL_KEY;
+import static com.github.herrschwarz.status4spring.SessionModelKeys.SESSION_ID_MODEL_KEY;
+import static com.github.herrschwarz.status4spring.ViewNames.INTERNAL_BUILD_VIEW_NAME;
+import static com.github.herrschwarz.status4spring.ViewNames.INTERNAL_SESSION_VIEW_NAME;
+import static com.github.herrschwarz.status4spring.ViewNames.INTERNAL_STATUS_VIEW_NAME;
+import static com.github.herrschwarz.status4spring.ViewNames.INTERNAL_VERSION_VIEW_NAME;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsMapContaining.hasKey;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.github.herrschwarz.status4spring.cache.CacheStatsProvider;
 import com.github.herrschwarz.status4spring.groups.UnitTest;
+import com.github.herrschwarz.status4spring.inspectors.HealthInspector;
+import com.github.herrschwarz.status4spring.inspectors.InspectionResult;
+
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
-import static com.github.herrschwarz.status4spring.SessionModelKeys.*;
-import static com.github.herrschwarz.status4spring.ViewNames.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.collection.IsMapContaining.hasKey;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNot.not;
-import static org.hamcrest.core.IsNull.nullValue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import javax.servlet.http.HttpSession;
 
 @Category(UnitTest.class)
 public class StatusControllerTest {
 
     public static final String ATTRIBUTE_KEY = "meaningOfDeath";
     public static final String ATTRIBUTE_VALUE = "666";
+    private static final String INSPECTOR_NAME = "inspector gadget";
+    private static final boolean INSPECTION_RESULT = true;
+    private static final String INSPECTION_DESCRIPTION = "description";
 
     @Test
     public void shouldSelectStatusViewNameForStatusPage() throws Exception {
@@ -131,5 +147,26 @@ public class StatusControllerTest {
         statusController.setCacheStatsProvider(cacheStatsProviderMock);
         statusController.clearCache("test");
         verify(cacheStatsProviderMock, times(1)).clearCache("test");
+    }
+
+    @Test
+    public void shouldCallHealthInspectors() {
+        // Given
+        StatusController statusController = new StatusController();
+        HealthInspector inspector = mock(HealthInspector.class);
+        statusController.addHealthInspector(inspector);
+        InspectionResult result = new InspectionResult(INSPECTOR_NAME, INSPECTION_RESULT, INSPECTION_DESCRIPTION);
+        when(inspector.inspect()).thenReturn(result);
+
+        // When
+        List<InspectionResult> inspectionResults = statusController.inspectSystem();
+
+        // Then
+        assertThat(inspectionResults, is(notNullValue()));
+        assertThat(inspectionResults, hasSize(1));
+        InspectionResult inspectionResult = inspectionResults.get(0);
+        assertThat(inspectionResult.getName(), is(INSPECTOR_NAME));
+        assertThat(inspectionResult.isSuccessful(), is(INSPECTION_RESULT));
+        assertThat(inspectionResult.getDescription(), is(INSPECTION_DESCRIPTION));
     }
 }
