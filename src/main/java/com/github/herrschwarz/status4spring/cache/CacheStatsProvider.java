a3c37afdf19651558b2767d66be6b0b772618daf
Felix Eckhardt <HerrSchwarz@users.noreply.github.com>
2016-06-01 23:23:51 +0200

    Feature/guava cache (#27)
    * GuavaCacheStatsProvider
    
    * test improvement
    
    * unit test annotation added
    
    * removed unwanted throws Exception
    
    * removed unwanted throws Exception
    
    * removed unwanted throws Exception
    
    * be more specific in throws Exception
    
    * removed unwanted throws Exception
    
    * Guava Cache Statistics
    
    * Create cirlce.yml

--- src/main/java/com/github/herrschwarz/status4spring/cache/CacheStatsProvider.java
+++ src/main/java/com/github/herrschwarz/status4spring/cache/CacheStatsProvider.java
@@ -1,10 +1,22 @@
 package com.github.herrschwarz.status4spring.cache;
 
+import org.springframework.cache.CacheManager;
+
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public interface CacheStatsProvider {
 
-    public List<CacheStats> getCacheStats();
-    public Long clearCache(String name);
+    CacheStats computeStats(String name);
+    Long clearCache(String name);
+    CacheManager getCacheManager();
+
+    default List<CacheStats> getCacheStats() {
+        return getCacheManager().getCacheNames()
+                .stream()
+                .map(this::computeStats)
+                .collect(toList());
+    }
 
 }
--- src/main/java/com/github/herrschwarz/status4spring/cache/ConcurrentMapCacheStatsProvider.java
+++ src/main/java/com/github/herrschwarz/status4spring/cache/ConcurrentMapCacheStatsProvider.java
@@ -1,46 +1,44 @@
 package com.github.herrschwarz.status4spring.cache;
 
 import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-import static java.util.stream.Collectors.toList;
+import static com.github.herrschwarz.status4spring.cache.ImmutableCacheStats.builder;
 
 public class ConcurrentMapCacheStatsProvider implements CacheStatsProvider {
 
-    private ConcurrentMapCacheManager cacheManager;
-    private static final String TYPE = "Concurrent map cache";
+  private ConcurrentMapCacheManager cacheManager;
+  private static final String TYPE = "Concurrent map cache";
 
-    public ConcurrentMapCacheStatsProvider(ConcurrentMapCacheManager cacheManager) {
-        this.cacheManager = cacheManager;
-    }
+  public ConcurrentMapCacheStatsProvider(ConcurrentMapCacheManager cacheManager) {
+    this.cacheManager = cacheManager;
+  }
 
-    @Override
-    public List<CacheStats> getCacheStats() {
-        return cacheManager.getCacheNames()
-                .stream()
-                .map(this::computeStats)
-                .collect(toList());
-    }
+  @Override
+  public Long clearCache(String name) {
+    Cache cache = cacheManager.getCache(name);
+    Long count = ((ConcurrentHashMap) cache.getNativeCache()).mappingCount();
+    cache.clear();
+    return count;
+  }
 
-    @Override
-    public Long clearCache(String name) {
-        Cache cache = cacheManager.getCache(name);
-        Long count = ((ConcurrentHashMap) cache.getNativeCache()).mappingCount();
-        cache.clear();
-        return count;
-    }
+  @Override
+  public CacheManager getCacheManager() {
+    return cacheManager;
+  }
 
-    private CacheStats computeStats(String name) {
-        ConcurrentMapCache cache = (ConcurrentMapCache) cacheManager.getCache(name);
-        CacheStats cacheStats = new CacheStats();
-        cacheStats.setName(name);
-        cacheStats.setType(TYPE);
-        Long count = ((ConcurrentHashMap) cache.getNativeCache()).mappingCount();
-        cacheStats.setNumberOfEntries(count);
-        return cacheStats;
-    }
+  @Override
+  public CacheStats computeStats(String name) {
+    ConcurrentMapCache cache = (ConcurrentMapCache) cacheManager.getCache(name);
+    Long count = ((ConcurrentHashMap) cache.getNativeCache()).mappingCount();
+    return builder()
+        .name(name)
+        .type(TYPE)
+        .numberOfEntries(count)
+        .build();
+  }
 }
